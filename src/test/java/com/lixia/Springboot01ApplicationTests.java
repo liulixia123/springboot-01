90b85d6785563da8fa480e34de7f3c93154e2b71
star_xing <33078552+liulixia123@users.noreply.github.com>
2021-04-18 14:47:05 +0800

    Update Springboot01ApplicationTests.java

--- src/test/java/com/lixia/Springboot01ApplicationTests.java
+++ src/test/java/com/lixia/Springboot01ApplicationTests.java
@@ -5,6 +5,9 @@ import com.lixia.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -16,6 +19,9 @@ class Springboot01ApplicationTests {
     private Person person;
     @Autowired
     DataSource dataSource;
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
     @Test
     void contextLoads() throws SQLException {
         //查看默认数据源
@@ -26,6 +32,14 @@ class Springboot01ApplicationTests {
 
         //连接关闭
         connection.close();
+        //一个简单的邮件
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setSubject("你好呀");
+        mailMessage.setText("欢迎你");
+        mailMessage.setTo("1518545702@qq.com");
+        mailMessage.setFrom("1518545702@qq.com");
+        mailSender.send(mailMessage);
+
     }
 
 }
