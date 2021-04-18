07e959fbe9ac800d4ad6cc34bdd879d8fff106e1
star_xing <33078552+liulixia123@users.noreply.github.com>
2021-04-18 15:46:57 +0800

    Update Springboot01Application.java

--- src/main/java/com/lixia/Springboot01Application.java
+++ src/main/java/com/lixia/Springboot01Application.java
@@ -3,7 +3,9 @@ package com.lixia;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 //@MapperScans("com.lixia.mapper")
 public class Springboot01Application {
